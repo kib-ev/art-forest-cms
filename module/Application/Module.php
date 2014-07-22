b7f9021d6e1c2b77a9de5785a32160f62dccc5ae
ardfo <ardfo@gmail.com>
2014-07-23 02:23:11 +0300

    edit Application Module.php

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
@@ -33,6 +25,15 @@ class Module
     // Application/config/module.config.php must be setted locale ru_RU
     protected function setDefaultTranslator($e)
     {
+        
+//        if (empty($_GET["lang"])) { // todo multilanguages
+//            $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
+//                $controller = $e->getTarget();
+//                $uri = 'http://zend.local' . $_SERVER["REQUEST_URI"] . '?lang=ru';
+//                $controller->plugin('redirect')->toURL($uri);
+//            });
+//        }
+
         $translator = $e->getApplication()->getServiceManager()->get('translator');
 
         $type = 'phpArray';
@@ -67,17 +68,13 @@ class Module
             'factories' => array(
                 // the array key here is the name you will call the view helper by in your view scripts
                 'getServiceManager' => function($vhm) {
-                    $sm = $vhm->getServiceLocator(); // $vhm is the view helper manager, so we need to fetch the main service manager
-                    return new \Application\View\Helper\ServiceManager($sm);
-                },
+            $sm = $vhm->getServiceLocator(); // $vhm is the view helper manager, so we need to fetch the main service manager
+            return new \Application\View\Helper\ServiceManager($sm);
+        },
                 'thumb' => function($vhm) {
-                    $sm = $vhm->getServiceLocator(); // $vhm is the view helper manager, so we need to fetch the main service manager
-                    return new \Application\View\Helper\Thumb($sm);
-                },
-                'getTrustEntity' => function($vhm) {
-                    $sm = $vhm->getServiceLocator(); // $vhm is the view helper manager, so we need to fetch the main service manager
-                    return new \Application\View\Helper\Trust($sm);
-                },
+            $sm = $vhm->getServiceLocator(); // $vhm is the view helper manager, so we need to fetch the main service manager
+            return new \Application\View\Helper\Thumb($sm);
+        },
             ),
         );
     }
