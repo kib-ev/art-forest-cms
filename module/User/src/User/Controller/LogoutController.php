a37a5cd8f7a3ed782483ef2a51a732ef3a7599c1
ardfo <ardfo@gmail.com>
2014-07-23 23:37:58 +0300

    refactor User module

--- module/User/src/User/Controller/LogoutController.php
+++ module/User/src/User/Controller/LogoutController.php
@@ -7,7 +7,21 @@ use Zend\Mvc\Controller\AbstractActionController;
 class LogoutController extends AbstractActionController {
 
     public function logoutAction() {
-        $this->getServiceLocator()->get('zend_auth_service')->clearIdentity();
-        return $this->redirect()->toUrl('/user/login');
+        $sm = $this->getServiceLocator();
+        $userId = $sm->get('logged_in_user_id');
+        if ($userId == 0) {
+            return $this->redirect()->toUrl('/user/logout/error');
+        } else {
+            $sm->get('zend_auth_service')->clearIdentity();
+            return $this->redirect()->toUrl('/user/logout/confirm');
+        }
+    }
+
+    public function confirmAction() {
+        return array();
+    }
+
+    public function errorAction() {
+        return array();
     }
 }
