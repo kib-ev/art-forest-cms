f2421d70824d379f6b987e9344a7a99bf44be99f
kib-ev <ardfo@gmail.com>
2014-08-05 02:12:42 +0300

    small fix

--- module/User/src/User/Model/UserTable.php
+++ module/User/src/User/Model/UserTable.php
@@ -12,31 +12,44 @@ class UserTable {
         $this->tableGateway = $tableGateway;
     }
 
-    public function getUserById($id) {
-        $id = (int) $id;
-        $rowset = $this->tableGateway->select(array('id' => $id));
+    /**
+     * 
+     * @param int $userId
+     * @return \Zend\Db\ResultSet\ResultSet $row or null
+     */
+    public function getUserById($userId) {
+        $userId = (int) $userId;
+        $rowset = $this->tableGateway->select(array('id' => $userId));
         $row = $rowset->current();
         return $row ? $row : null;
     }
 
+    /**
+     * 
+     * @param text $email
+     * @return \Zend\Db\ResultSet\ResultSet $row or null
+     */
     public function getUserByEmail($email) {
         $rowset = $this->tableGateway->select(array('email' => $email));
         $row = $rowset->current();
         return $row ? $row : null;
     }
 
+    /**
+     * 
+     * @param \User\Model\User $user
+     */
     public function saveUser(User $user) {
-
         $data = $user->getArrayCopy();
+
         unset($data['id']);
+        $userId = (int) $user->get('id');
 
-        $id = (int) $user->get('id');
-
-        if ($id == 0) {
+        if ($userId == 0) {
             $this->tableGateway->insert($data);
         } else {
-            if ($this->getUser($id)) {
-                $this->tableGateway->update($data, array('user_id' => $id));
+            if ($this->getUserById($userId)) {
+                $this->tableGateway->update($data, array('id' => $userId));
             }
         }
     }
